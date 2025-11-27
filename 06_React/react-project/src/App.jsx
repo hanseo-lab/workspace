edf56fd1033c32e9ce664084a93547488c8fc478
hanseo-lab <hanseo.lab@gmail.com>
2025-11-27 12:45:30 +0900

    2025.11.27 수업내용 -customHook, useContext, useEffect

--- 06_React/react-project/src/App.jsx
+++ 06_React/react-project/src/App.jsx
@@ -2,14 +2,40 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import UseStateTest from './components/useState/UseStateTest'
+import Signup from './components/useState/Signup'
+import LandingPage from './components/useState/LandingPage'
+import UseRefTest from './components/useRef/UseRefTest'
+import UseRefScroll from './components/useRef/UseRefScroll'
+import UseMemoTest from './components/useMemo/UseMemoTest'
+import UseCallbackTest from './components/useCallback/UseCallbackTest'
+import UseEffectView from './components/useEffect/UseEffectView'
+import UseContextTest from './components/useContext/UseContextTest'
+import MyInfo from './components/customHook/MyInfo'
+import ToggleBox from './components/customHook/ToggleBox'
+import { UserProvider } from './components/useContext/UserContext'
+import Header from './components/useContext/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
-    </>
+      {/* <UseStateTest /> */}
+      {/* <Signup /> */}
+      {/* <LandingPage /> */}
+      {/* <UseRefTest /> */}
+      {/* <UseRefScroll /> */}
+      {/* <UseMemoTest /> */}
+      {/* <UseCallbackTest /> */}
+      {/* <UseEffectView /> */}
+      {/* <UseContextTest /> */}
+      {/* <MyInfo /> */}
+      {/* <ToggleBox /> */}
+      <UserProvider>
+        <Header />
+      </UserProvider>
+
+    </> 
   )
 }
 
